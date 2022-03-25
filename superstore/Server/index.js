fea516d94fc6436ec2a548fa88d57fc54a04b252
suvratkumra <suvratkumra2018@gmail.com>
2022-03-25 00:38:26 -0600

    Adding the backend to take the signup information and store it in database

--- superstore/Server/index.js
+++ superstore/Server/index.js
@@ -5,6 +5,8 @@
 // use the command "npm run devStart"
 //creating an express server
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const mysql = require('mysql')      // to get the mysql dependency
 
@@ -15,15 +17,29 @@ const db = mysql.createConnection({
     database: 'superstore_database'             // this is the name of the table
 })
 
+// this is the middle ware, you need this, without this, alot of errors.
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.json())     // allows to get the request from frontend.
+
 // res is to send, THE RESPONSE, to send it to the front end.
 // req is require, REQUIRE, get information from the front end.
 app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO customer(Email, minAge, maxAge, customer_name) VALUES ('suvrat.kumra@ucalgary.ca', 10, 20, 'Suvrat');";
+   
+})
+
+app.post('/signup/insert', (req, res) => {
+
+    // requesting information from front end
+    const Username = req.body.Username
+    const Email_Id = req.body.Email_Id
+    const Password = req.body.Password
+
+    const sqlInsert = "INSERT INTO login_details(Username, Email_Id, Password) VALUES (?,?,?)";
     
-    db.query(sqlInsert, (err, result) => {
-        res.send("The new information added.")
-    })
-    res.send('hello world');
+    db.query(sqlInsert, [Username, Email_Id, Password], (err, result) => {
+
+    });
 })
 
 // to listen
