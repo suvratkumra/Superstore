d2ca3c1e1c0b46b7cd4dbd78c9b79ae8affaa8b5
shreyap5 <60752941+shreyap5@users.noreply.github.com>
2022-03-25 15:07:11 -0600

    Updated (working)

--- superstore/Client/src/App.js
+++ superstore/Client/src/App.js
@@ -1,33 +1,42 @@
+import "./App.css";
+import styled from "styled-components";
+import { AccountBox } from "./components";
+
 import React from 'react';
-import login from './components/login.js';
 import {Container, Button, View} from "react-bootstrap";
 import logo from "./images/logo.png";
 import {Helmet} from 'react-helmet';
 
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: bottom;
+  justify-content: center;
+`;
+
+
 function App() {
   return (
     <>
+  
     <div className="application">
             <Helmet>
                 <style>{'body { background-color: salmon; }'}</style>
             </Helmet>
         </div>
     <div>
-      <text style={{fontSize:40, backgroundColor:'darkslateblue', width:2000, marginLeft:600, color:'white' }}>Welcome To Superstore</text>
+      <text style={{fontSize:40, backgroundColor:'darkslateblue', width:50, marginLeft:600, color:'white' }}>Welcome To Superstore</text>
     </div>
     <img src={logo} alt="logo_superstore" width={300}/>
 
     <span className="block-example border border-primary"></span>
 
- 
 
-    <Container style={{flex:1, flexDirection:'column', alignItems:'center', justifyContent:'space-between', marginLeft:650}}>
-      
-      <Button style={{width:200,backgroundColor:'teal',marginTop:180,borderWidth: 4,borderColor: "#20232a"}}>
-        <text style={{marginLeft:10, fontSize:40}}>LOGIN</text>
-      </Button>
-    </Container>
-
+    <AppContainer>
+      <AccountBox />
+    </AppContainer>
     <Container style={{flex:1, flexDirection:'column', alignItems:'center', justifyContent:'space-between', marginLeft:675}}>
       <div className='Guest_link'>
         <a href="https://www.realcanadiansuperstore.ca/" target="_blank" rel="noreferrer noopener">
@@ -39,18 +48,11 @@ function App() {
     </Container>
 
 
-    <Container style={{flex:1, flexDirection:'column', alignItems:'center', justifyContent:'space-between', marginLeft:650}}>
-    
-      <Button style={{width:200,backgroundColor:'teal',marginTop:70, borderWidth: 4,borderColor: "#20232a"}}>
-        <text style={{marginLeft:10, fontSize:40}}>SIGN UP</text>
-      </Button>
-    </Container>
-
     
     <Container style={{flex:1, flexDirection:'column', alignItems:'center', justifyContent:'space-between', marginLeft:1200}}>
     
     <Button style={{width:300,backgroundColor:'navy',marginTop:200, borderWidth: 4,borderColor: "#20232a", color: "white"}}>
-      <text style={{marginLeft:10, fontSize:20}}>Contiue as a Manager</text>
+      <text style={{marginLeft:10, fontSize:20}}>Continue as a Manager</text>
     </Button>
   </Container>
 
---
+++ superstore/Client/src/components/loginForm.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import {
+  BoldLink,
+  BoxContainer,
+  FormContainer,
+  Input,
+  MutedLink,
+  SubmitButton,
+} from "./common";
+
+import { AccountContext } from "./accountContext";
+
+export function LoginForm(props) {
+  const { switchToSignup } = useContext(AccountContext);
+
+  return (
+    <BoxContainer>
+      <FormContainer>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+      </FormContainer>
+      <MutedLink href="#">Forget your password?</MutedLink>
+      <SubmitButton type="submit">Signin</SubmitButton>
+
+      <MutedLink href="#">
+        Create an account{" "}
+        <BoldLink href="#" onClick={switchToSignup}>
+          Signup
+        </BoldLink>
+      </MutedLink>
+    </BoxContainer>
+  );
+}
