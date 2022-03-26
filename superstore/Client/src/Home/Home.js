c24d7e404c7e9b0f5b13ee5dab7ed78efbc0a266
suvratkumra <suvratkumra2018@gmail.com>
2022-03-25 21:21:02 -0600

    customizing the home page

--- superstore/Client/src/Home/Home.js
+++ superstore/Client/src/Home/Home.js
@@ -2,9 +2,32 @@ import React from 'react';
 import '../css/Home.css';     // two dots because you have to go to previous directory first
 export default function WelcomePage() {
   return (
-    <div>
-        <span class = "superstore_name">WELCOME TO SUPERSTORE</span>
-        <button onClick = {event => window.location.href='/signup'}>signup</button>
+    <>
+    <div className='name__container'>
+        <span class = "superstore_name">WELCOME </span>
+        <span class = "superstore_name"> TO </span>
+        <span class = "superstore_name">SUPERSTORE</span>
     </div>
+
+
+    {/* Now making the login page */}
+    <div className = 'login__container'>
+      <div className='login__containerHeader'>
+        A registered user? Login below...
+      </div>
+      <div className='login__credentials'>
+        
+      </div>
+    </div>
+
+
+
+
+    {/* <div className='buttons__container'>
+       <button onClick = {event => window.location.href='/signup'}>signup</button>
+    </div> */}
+    </>
+    
+    
   )
 }
