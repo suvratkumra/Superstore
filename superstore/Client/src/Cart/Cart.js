0ae3b9eca2f008e382cff85a219e2574456e9aff
suvratkumra <suvratkumra2018@gmail.com>
2022-04-13 17:20:07 -0600

    Completing cart screen

--- superstore/Client/src/Cart/Cart.js
+++ superstore/Client/src/Cart/Cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './Cart.js.css';     // two dots because you have to go to previous directory first
 import Axios from 'axios'
+import PaymentMethod from '../images/PaymentMethod.png'
 
 export default function Cart() {
     
@@ -10,17 +11,19 @@ export default function Cart() {
     const [resultStore, setResultStore] = useState({});
     const [isEmail, setIsEmail] = useState(false)
     const [isObject, setIsObject] = useState(false)
+    // const [totalBill, setTotalBill] = useState(0.00)
+  var totalBill = 0;
 
 
 useEffect(() => {
     // function getEmail(){
         Axios.post("http://localhost:3001/api/getEmail"
         ).then((res) => {
-          console.log(res.data);
+          // console.log(res.data);
           setEmail(res.data)
-          console.log(email);
+          // console.log(email);
           setIsEmail(true);
-          console.log(email.length);
+          // console.log(email.length);
           if(res.length === 0) {
             setContinuing(false);
           }else {
@@ -35,10 +38,10 @@ useEffect(() => {
         Axios.post("http://localhost:3001/api/getCart", {
             email: email
         }).then((res) => {
-            console.log(res.data)
+            // console.log(res.data)
             setResultStore(res.data);
             setIsObject(true);
-            console.log(resultStore)
+            // console.log(resultStore)
         })
     // }
 }, [isEmail, isObject])
@@ -63,18 +66,53 @@ useEffect(() => {
             <span className='cart__Heading'>YOUR CART</span>
         </div>
         <div id = "products_and_prices__container">
+          <div id = "heading__container_cart">
+            <span>Product Name</span>
+            <span>Price</span>
+            <span>Quantity</span>
+
+          </div>
+        
+        <br/>
             <div id = "products_section__container">
-            {/* {getCartItems()} */}
-            {/* { Object.values(resultStore).map((item, value) => {
-            return  
-                      <span id = "itemName__name">{item}</span>
-            
-            }) } */}
+            {/* {console.log(resultStore)} */}
+
+            { Object.values(resultStore).map((item, value) => {
+            return  <div id = "item_displaying__container">              
+              <span id = "itemName__name">{Object.values(item).at(1)}</span>
+              <span id = "itemName__name">${(Object.values(item).at(2)*Object.values(item).at(3)).toFixed(2)}</span>
+              <span id = "itemName__name">{Object.values(item).at(3)}</span>
+                
+            </div>
+            }) }
             </div>
 
-            <div id = "price_sectino__container">
+            { Object.values(resultStore).map((item, value) => {
+              totalBill+=parseFloat((Object.values(item).at(2)*Object.values(item).at(3)).toFixed(2))
+              // console.log((totalBill));
+            })}
 
-                
+            <div id = "price_section__container">
+                <span id = "textBill__text">YOUR BILL</span>
+                <hr/>
+                <br/>
+                <span id = "totalSubBill__text">Your Subtotal: ${totalBill.toFixed(2)} <br/> + <br/> GST: ${((totalBill.toFixed(2)/100)*5).toFixed(2)}</span>
+                <br/>
+                <hr/>
+                <br/>
+                <span id = "totalBill__text">Total: ${(parseFloat(totalBill.toFixed(2)) + parseFloat((totalBill.toFixed(2)/100)*5)).toFixed(2)}</span>
+                <br/>
+                <div id = "payUsing__text">
+                  Pay Using:<br/><br/>
+                  <div id = "buttons__payment">
+                    <button className= "button-29" onClick = {"#"}>CRYPTO</button>
+                    <button className= "button-29" onClick = {"#"}>VISA</button>
+                    <button className= "button-29" onClick = {"#"}>INSTADEBIT</button>
+                    <button className= "button-29" onClick = {"#"}>PAYPAL</button>
+                    <button className= "button-29" onClick = {"#"}>MASTERCARD</button>
+                  </div>
+                  
+                  </div>
             </div>
         </div>
 
