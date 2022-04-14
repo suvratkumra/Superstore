689ee9d30e211171705c1bd3d0fdba42ad138472
shreyap5 <60752941+shreyap5@users.noreply.github.com>
2022-04-14 15:17:03 -0600

    updated regex

--- superstore/Client/src/HelpPages/RegEx.js
+++ superstore/Client/src/HelpPages/RegEx.js
@@ -1,6 +1,6 @@
 export const validEmail = new RegExp(
     '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
  );
- export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
+ export const validPassword = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&-]).{5,}$');
  export const validUsername = new RegExp('^(?=[a-zA-Z0-9._]{4,20}$)(?!.*[_.]{2})[^_.].*[^_.]$');
  export const validAnswer = new RegExp('^[a-zA-Z0-9]{4,20}$')
